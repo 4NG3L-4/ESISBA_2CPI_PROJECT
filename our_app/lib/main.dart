781d3efd117c807214026021fa1602d33c62077e
4NG3L-4 <a.dahmane@esi-sba.dz>
2021-05-25 10:27:39 +0100

    Auth.dart update

--- our_app/lib/main.dart
+++ our_app/lib/main.dart
@@ -32,7 +32,7 @@ class MyHomePage extends StatefulWidget {
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields
-   that affect
+  //that affect
   // how it looks.
 
   // This class is the configuration for the state. It holds the values (in this
