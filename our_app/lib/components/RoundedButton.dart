17b2e41db23d81a4e8215457e8380f91453fb291
ines-bld <i.belouad@esi-sba.dz>
2021-06-06 18:01:24 +0100

    roundedbuttom update

--- our_app/lib/components/RoundedButton.dart
+++ our_app/lib/components/RoundedButton.dart
@@ -5,12 +5,14 @@ class RoundedButton extends StatelessWidget {
   final String text;
   final Function press;
   final Color color, textColor;
+  final double sizee;
   const RoundedButton({
     Key key,
     this.text,
     this.press,
     this.color = myDarkBlue,
     this.textColor = Colors.white,
+    this.sizee,
   }) : super(key: key);
 
   @override
@@ -18,9 +20,9 @@ class RoundedButton extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
-      width: size.width * 0.8,
+      width: size.width * 0.7,
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(29),
+        borderRadius: BorderRadius.circular(30),
         // ignore: deprecated_member_use
         child: FlatButton(
           padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
@@ -28,7 +30,10 @@ class RoundedButton extends StatelessWidget {
           onPressed: press,
           child: Text(
             text,
-            style: TextStyle(color: textColor),
+            style: TextStyle(
+              color: textColor,
+              fontSize: sizee,
+            ),
           ),
         ),
       ),
