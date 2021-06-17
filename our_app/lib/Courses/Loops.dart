2d9f596db30f1d8247a9b02ce3086e9d9d615e48
Manar262001 <m.sahel@esi-sba.dz>
2021-06-18 00:03:56 +0100

    changes principal

---
+++ our_app/lib/Courses/Loops.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoopsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Loops course!"),
+      ),
+    );
+  }
+}
