c962bea8e90371d287c19cd0ec9ef0dabc72c266
Manar262001 <m.sahel@esi-sba.dz>
2021-06-30 22:05:31 +0100

    cc

--- our_app/lib/Screens/Login/components/body.dart
+++ our_app/lib/Screens/Login/components/body.dart
@@ -29,62 +29,67 @@ class _LoginPageState extends State<LoginPage> {
 
   String email, password;
   Widget _buildEmailRow() {
-    return Padding(
-      padding: EdgeInsets.all(0.025),
-      child: TextFormField(
-        controller: _emailController,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        cursorColor: myLightBlue,
-        decoration: InputDecoration(
-          labelText: 'Email',
-          labelStyle: TextStyle(
-            color: myLightBlue,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(0.025),
+        child: SingleChildScrollView(
+          child: TextFormField(
+            controller: _emailController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            cursorColor: myLightBlue,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              labelStyle: TextStyle(
+                color: myLightBlue,
+              ),
+            ),
+            keyboardType: TextInputType.emailAddress,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Enter your email please';
+              }
+              if (!RegExp(
+                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                  .hasMatch(value)) {
+                return 'Invalid Email';
+              }
+              return null;
+            },
+            onSaved: (value) {},
           ),
         ),
-        keyboardType: TextInputType.emailAddress,
-        validator: (value) {
-          if (value.isEmpty) {
-            return 'Enter your email please';
-          }
-          if (!RegExp(
-                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-              .hasMatch(value)) {
-            return 'Invalid Email';
-          }
-          return null;
-        },
-        onSaved: (value) {},
+        // SizedBox(height: 20, width: 20),
       ),
-
-      // SizedBox(height: 20, width: 20),
     );
   }
 
   Widget _buildPasswordRow() {
-    return Padding(
-      padding: EdgeInsets.all(0.0001),
-      child: Form(
-        child: TextFormField(
-          controller: _passwordController,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          cursorColor: myLightBlue,
-          decoration: InputDecoration(
-            labelText: 'password',
-            labelStyle: TextStyle(
-              color: myLightBlue,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(0.0001),
+        child: Form(
+          child: TextFormField(
+            controller: _passwordController,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            cursorColor: myLightBlue,
+            decoration: InputDecoration(
+              labelText: 'password',
+              labelStyle: TextStyle(
+                color: myLightBlue,
+              ),
             ),
+            obscureText: true,
+            validator: (value) {
+              if (value.isEmpty || value.length < 5) {
+                return 'password must be more than 4 characters';
+              }
+              return null;
+            },
+            onSaved: (value) {},
           ),
-          obscureText: true,
-          validator: (value) {
-            if (value.isEmpty || value.length < 5) {
-              return 'password must be more than 4 characters';
-            }
-            return null;
-          },
-          onSaved: (value) {},
         ),
+        //SizedBox(height: 20, width: 20),
       ),
-      //SizedBox(height: 20, width: 20),
     );
   }
 
@@ -111,51 +116,53 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _buildLoginButton() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Container(
-            height: 1.4 * (MediaQuery.of(context).size.height / 20),
-            width: 5 * (MediaQuery.of(context).size.width / 10),
-            margin: EdgeInsets.all(12),
-            // ignore: deprecated_member_use
+    return SingleChildScrollView(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(4.0),
             child: Container(
-              child: RaisedButton(
-                elevation: 5.0,
-                color: myDarkBlue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                //add login function
-                onPressed: () {
-                  if (_key.currentState.validate()) {
-                    signInUser();
+              height: 1.4 * (MediaQuery.of(context).size.height / 20),
+              width: 5 * (MediaQuery.of(context).size.width / 10),
+              margin: EdgeInsets.all(12),
+              // ignore: deprecated_member_use
+              child: Container(
+                child: RaisedButton(
+                  elevation: 5.0,
+                  color: myDarkBlue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  //add login function
+                  onPressed: () {
+                    if (_key.currentState.validate()) {
+                      signInUser();
 
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return PrincipaleStScreen();
-                        },
-                      ),
-                    );
-                  }
-                },
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 1.5,
-                    fontSize: MediaQuery.of(context).size.height / 40,
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return PrincipaleScreen();
+                          },
+                        ),
+                      );
+                    }
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 1.5,
+                      fontSize: MediaQuery.of(context).size.height / 40,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ),
-      ],
+          )
+        ],
+      ),
     );
   }
 
@@ -178,52 +185,59 @@ class _LoginPageState extends State<LoginPage> {
                   decoration: BoxDecoration(
                     color: myLightWhite,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            "Login",
-                            style: TextStyle(
-                              fontSize: MediaQuery.of(context).size.height / 30,
-                            ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        SingleChildScrollView(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.height / 30,
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      _buildEmailRow(),
-                      _buildPasswordRow(),
-                      _buildForgetPasswordButton(),
-                      _buildLoginButton(),
-                      _buildSignUpBtn(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          SvgPicture.asset(
-                            "assets/icons/facebook.svg",
-                            height: 21,
-                            width: 30,
-                            color: myDarkBlue,
+                        ),
+                        _buildEmailRow(),
+                        _buildPasswordRow(),
+                        _buildForgetPasswordButton(),
+                        _buildLoginButton(),
+                        _buildSignUpBtn(),
+                        SingleChildScrollView(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SvgPicture.asset(
+                                "assets/icons/facebook.svg",
+                                height: 21,
+                                width: 30,
+                                color: myDarkBlue,
+                              ),
+                              SizedBox(width: 20),
+                              SvgPicture.asset(
+                                "assets/icons/instagram.svg",
+                                height: 21,
+                                width: 30,
+                                color: myDarkBlue,
+                              ),
+                              SizedBox(width: 20),
+                              SvgPicture.asset(
+                                "assets/icons/twitter.svg",
+                                height: 21,
+                                width: 30,
+                                color: myDarkBlue,
+                              ),
+                            ],
                           ),
-                          SizedBox(width: 20),
-                          SvgPicture.asset(
-                            "assets/icons/instagram.svg",
-                            height: 21,
-                            width: 30,
-                            color: myDarkBlue,
-                          ),
-                          SizedBox(width: 20),
-                          SvgPicture.asset(
-                            "assets/icons/twitter.svg",
-                            height: 21,
-                            width: 30,
-                            color: myDarkBlue,
-                          ),
-                        ],
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
                 )),
               ),
@@ -235,46 +249,57 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _buildSignUpBtn() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              // ignore: deprecated_member_use
-              child: FlatButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUpScreen()),
-                  );
-                },
-                child: RichText(
-                  text: TextSpan(children: [
-                    TextSpan(
-                      text: 'Dont have an account? ',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: MediaQuery.of(context).size.height / 40,
-                        fontWeight: FontWeight.w400,
+    return SingleChildScrollView(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Row(
+              children: [
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    // ignore: deprecated_member_use
+                    child: FlatButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignUpScreen()),
+                        );
+                      },
+                      child: SingleChildScrollView(
+                        child: RichText(
+                          text: TextSpan(children: [
+                            TextSpan(
+                              text: 'Dont have an account? ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize:
+                                    MediaQuery.of(context).size.height / 40,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            TextSpan(
+                              text: 'Sign Up',
+                              style: TextStyle(
+                                color: Color.fromRGBO(101, 141, 218, 1),
+                                fontSize:
+                                    MediaQuery.of(context).size.height / 40,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ]),
+                        ),
                       ),
                     ),
-                    TextSpan(
-                      text: 'Sign Up',
-                      style: TextStyle(
-                        color: Color.fromRGBO(101, 141, 218, 1),
-                        fontSize: MediaQuery.of(context).size.height / 40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )
-                  ]),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 
